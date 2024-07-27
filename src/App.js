110a2e372f08153ac9f52adb7dd1ae8759bfddfe
Jarret Horner <JHorn384@mtroyal.ca>
2024-07-27 13:37:48 -0600

    feat: changed API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [data, setData] = useState('')
+  const [data, setData] = useState('');
 
   useEffect(() => {
     (async function () {
-      const {text} = await( await fetch('/api/src/functions/message')).json();
+      const { text } = await( await fetch(`/api/message`)).json();
       setData(text);
     })();
-  })
+  });
+
   return <div>{data}</div>;
 }
 
