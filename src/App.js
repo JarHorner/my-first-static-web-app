4d96fd6b8ac0e2530e918abdaabd8df75424bf25
Jarret Horner <JHorn384@mtroyal.ca>
2024-07-27 21:58:06 -0600

    Changes

--- src/App.js
+++ src/App.js
@@ -25,63 +25,63 @@ function App() {
     console.table(result.value);
   }
 
-  async function update() {
-    const id = 1;
-    const data = {
-      FirstName: "Molly",
-    };
+  // async function update() {
+  //   const id = 1;
+  //   const data = {
+  //     FirstName: "Molly",
+  //   };
 
-    const endpoint = "/data-api/rest/Customer/Id";
-    const response = await fetch(`${endpoint}/${id}`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    });
-    const result = await response.json();
-    console.table(result.value);
-  }
+  //   const endpoint = "/data-api/rest/Customer/Id";
+  //   const response = await fetch(`${endpoint}/${id}`, {
+  //     method: "PUT",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify(data),
+  //   });
+  //   const result = await response.json();
+  //   console.table(result.value);
+  // }
 
-  async function create() {
-    const data = {
-      CustomerID: 161,
-      NameStyle: false,
-      Title: "Mr.",
-      FirstName: "Jarret",
-      MiddleName: "A.",
-      LastName: "Horner",
-      Suffix: null,
-      CompanyName: "Winners",
-      SalesPerson: "adventure-works\\pamela0",
-      EmailAddress: "jarret0@Winners.com",
-      Phone: "245-555-0173",
-      PasswordHash: "L/Rlwxzp4w7RWmEgXX+/A7cXaePEPcp+KwQhl2fJL7w=",
-      PasswordSalt: "1KjXYs4=",
-      rowguid: "3F5AE95E-B87D-4AED-95B4-C3797AFCB74F",
-      ModifiedDate: "2005-08-01T00:00:00",
-    };
+  // async function create() {
+  //   const data = {
+  //     CustomerID: 161,
+  //     NameStyle: false,
+  //     Title: "Mr.",
+  //     FirstName: "Jarret",
+  //     MiddleName: "A.",
+  //     LastName: "Horner",
+  //     Suffix: null,
+  //     CompanyName: "Winners",
+  //     SalesPerson: "adventure-works\\pamela0",
+  //     EmailAddress: "jarret0@Winners.com",
+  //     Phone: "245-555-0173",
+  //     PasswordHash: "L/Rlwxzp4w7RWmEgXX+/A7cXaePEPcp+KwQhl2fJL7w=",
+  //     PasswordSalt: "1KjXYs4=",
+  //     rowguid: "3F5AE95E-B87D-4AED-95B4-C3797AFCB74F",
+  //     ModifiedDate: "2005-08-01T00:00:00",
+  //   };
 
-    const endpoint = `/data-api/rest/Person/`;
-    const response = await fetch(endpoint, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    });
-    const result = await response.json();
-    console.table(result.value);
-  }
+  //   const endpoint = `/data-api/rest/Person/`;
+  //   const response = await fetch(endpoint, {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify(data),
+  //   });
+  //   const result = await response.json();
+  //   console.table(result.value);
+  // }
 
-  async function del() {
-    const id = 3;
-    const endpoint = "/data-api/rest/Customer/Id";
-    const response = await fetch(`${endpoint}/${id}`, {
-      method: "DELETE",
-    });
-    if (response.ok) {
-      console.log(`Record deleted: ${id}`);
-    } else {
-      console.log(response);
-    }
-  }
+  // async function del() {
+  //   const id = 3;
+  //   const endpoint = "/data-api/rest/Customer/Id";
+  //   const response = await fetch(`${endpoint}/${id}`, {
+  //     method: "DELETE",
+  //   });
+  //   if (response.ok) {
+  //     console.log(`Record deleted: ${id}`);
+  //   } else {
+  //     console.log(response);
+  //   }
+  // }
 
   return (
     <>
@@ -98,7 +98,7 @@ function App() {
         <button id="get" onclick={get()}>
           Get
         </button>
-        <button id="update" onclick={update()}>
+        {/* <button id="update" onclick={update()}>
           Update
         </button>
         <button id="create" onclick={create()}>
@@ -106,7 +106,7 @@ function App() {
         </button>
         <button id="delete" onclick={del()}>
           Delete
-        </button>
+        </button> */}
       </div>
     </>
   );
