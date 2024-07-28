0d1ca46316cce3fe0011c573c0df4f88752bf5b1
Jarret Horner <JHorn384@mtroyal.ca>
2024-07-27 21:46:59 -0600

    test fixing bug

--- src/App.js
+++ src/App.js
@@ -36,74 +36,75 @@ function App() {
         </button>
       </div>
       <script>
-        {async function list() {
-          const endpoint = "/data-api/rest/Customer";
-          const response = await fetch(endpoint);
-          const data = await response.json();
-          console.table(data.value);
-        }}
-        {async function get() {
-          const id = 1;
-          const endpoint = `/data-api/rest/Customer/Id`;
-          const response = await fetch(`${endpoint}/${id}`);
-          const result = await response.json();
-          console.table(result.value);
-        }}
-        {async function update() {
-          const id = 1;
-          const data = {
-            FirstName: "Molly"
-          };
-
-          const endpoint = "/data-api/rest/Customer/Id";
-          const response = await fetch(`${endpoint}/${id}`, {
-            method: "PUT",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data),
-          });
-          const result = await response.json();
-          console.table(result.value);
-        }}
-        {async function create() {
-          const data = {
-            CustomerID: 161,
-            NameStyle: false,
-            Title: "Mr.",
-            FirstName: "Jarret",
-            MiddleName: "A.",
-            LastName: "Horner",
-            Suffix: null,
-            CompanyName: "Winners",
-            SalesPerson: "adventure-works\\pamela0",
-            EmailAddress: "jarret0@Winners.com",
-            Phone: "245-555-0173",
-            PasswordHash: "L/Rlwxzp4w7RWmEgXX+/A7cXaePEPcp+KwQhl2fJL7w=",
-            PasswordSalt: "1KjXYs4=",
-            rowguid: "3F5AE95E-B87D-4AED-95B4-C3797AFCB74F",
-            ModifiedDate: "2005-08-01T00:00:00",
-          };
-
-          const endpoint = `/data-api/rest/Person/`;
-          const response = await fetch(endpoint, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(data),
-          });
-          const result = await response.json();
-          console.table(result.value);
-        }}
-        {async function del() {
-          const id = 3;
-          const endpoint = "/data-api/rest/Customer/Id";
-          const response = await fetch(`${endpoint}/${id}`, {
-            method: "DELETE",
-          });
-          if (response.ok) {
-            console.log(`Record deleted: ${id}`);
-          } else {
-            console.log(response);
+        {
+          async function list() {
+            const endpoint = "/data-api/rest/Customer";
+            const response = await fetch(endpoint);
+            const data = await response.json();
+            console.table(data.value);
           }
-        }}
+          // {async function get() {
+          //   const id = 1;
+          //   const endpoint = `/data-api/rest/Customer/CustomerId`;
+          //   const response = await fetch(`${endpoint}/${id}`);
+          //   const result = await response.json();
+          //   console.table(result.value);
+          // }}
+          // {async function update() {
+          //   const id = 1;
+          //   const data = {
+          //     FirstName: "Molly"
+          //   };
+
+          //   const endpoint = "/data-api/rest/Customer/Id";
+          //   const response = await fetch(`${endpoint}/${id}`, {
+          //     method: "PUT",
+          //     headers: { "Content-Type": "application/json" },
+          //     body: JSON.stringify(data),
+          //   });
+          //   const result = await response.json();
+          //   console.table(result.value);
+          // }}
+          // {async function create() {
+          //   const data = {
+          //     CustomerID: 161,
+          //     NameStyle: false,
+          //     Title: "Mr.",
+          //     FirstName: "Jarret",
+          //     MiddleName: "A.",
+          //     LastName: "Horner",
+          //     Suffix: null,
+          //     CompanyName: "Winners",
+          //     SalesPerson: "adventure-works\\pamela0",
+          //     EmailAddress: "jarret0@Winners.com",
+          //     Phone: "245-555-0173",
+          //     PasswordHash: "L/Rlwxzp4w7RWmEgXX+/A7cXaePEPcp+KwQhl2fJL7w=",
+          //     PasswordSalt: "1KjXYs4=",
+          //     rowguid: "3F5AE95E-B87D-4AED-95B4-C3797AFCB74F",
+          //     ModifiedDate: "2005-08-01T00:00:00",
+          //   };
+
+          //   const endpoint = `/data-api/rest/Person/`;
+          //   const response = await fetch(endpoint, {
+          //     method: "POST",
+          //     headers: { "Content-Type": "application/json" },
+          //     body: JSON.stringify(data),
+          //   });
+          //   const result = await response.json();
+          //   console.table(result.value);
+          // }}
+          // {async function del() {
+          //   const id = 3;
+          //   const endpoint = "/data-api/rest/Customer/Id";
+          //   const response = await fetch(`${endpoint}/${id}`, {
+          //     method: "DELETE",
+          //   });
+          //   if (response.ok) {
+          //     console.log(`Record deleted: ${id}`);
+          //   } else {
+          //     console.log(response);
+          //   }
+        }
       </script>
     </>
   );
