8eefd525cf7f2059ba8c19d86e849c188da3aaa0
Jarret Horner <JHorn384@mtroyal.ca>
2024-07-28 22:58:25 -0600

    edits

--- src/apiService.js
+++ src/apiService.js
@@ -1,4 +1,4 @@
-// src/apiService.js
+
 export const fetchData = async () => {
     const response = await fetch('https://my-first-node-web-app.azurewebsites.net/api/data');
     if (!response.ok) {
