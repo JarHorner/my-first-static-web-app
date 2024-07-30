5560b0cbd9a68af671c63fb2610ebc3e932c5002
Jarret Horner <JHorn384@mtroyal.ca>
2024-07-30 13:28:38 -0600

    Added POST and DELETE functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { fetchCustomers, updateCustomerFirstName } from "./apiService";
+import { fetchCustomers, updateCustomerFirstName, addCustomer, removeCustomer } from "./apiService";
 
 function App() {
   const [customers, setCustomers] = useState([]);
@@ -7,8 +7,18 @@ function App() {
   const [order, setOrder] = useState("asc");
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(10);
+
   const [newFirstName, setNewFirstName] = useState("");
   const [customerIdToUpdate, setCustomerIdToUpdate] = useState("");
+
+  const [addFirstName, setAddFirstName] = useState("");
+  const [addLastName, setAddLastName] = useState("");
+  const [addCompany, setAddCompany] = useState("");
+  const [addEmail, setAddEmail] = useState("");
+  const [addPhone, setAddPhone] = useState("");
+
+  const [removeCustomerId, setRemoveCustomerId] = useState("");
+  const [removeFirstName, setRemoveFirstName] = useState("");
 
   useEffect(() => {
     fetchCustomers(sortBy, order, page, limit)
@@ -27,6 +37,39 @@ function App() {
     updateCustomerFirstName(customerIdToUpdate, newFirstName);
   };
 
+  async function addNewCustomer() {
+
+    if (!addFirstName || !addLastName) {
+      alert("Please provide the customers first and last name");
+      return;
+    }
+
+    addCustomer(addFirstName, addFirstName, addCompany, addEmail, addPhone);
+  }
+
+  async function removeExistingCustomer() {
+
+    if (!removeCustomerId || !removeFirstName) {
+      alert("Please provide the customers first and last name");
+      return;
+    }
+
+    removeCustomer(removeCustomerId, removeFirstName);
+  }
+
+  async function del() {
+    const id = 161;
+    const endpoint = "/data-api/rest/Customer/CustomerID";
+    const response = await fetch(`${endpoint}/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      console.log(`Record deleted: ${id}`);
+    } else {
+      console.log(response);
+    }
+  }
+
   return (
     <>
       <h1>Static Web Apps Database Connections</h1>
@@ -37,7 +80,7 @@ function App() {
         </button>
         <ul>
           {customers.map((customer) => (
-            <li key={customer.CustomerID}>{customer.FirstName}</li>
+            <li key={customer.CustomerID}>{customer.CustomerID} FirstName: {customer.FirstName}</li>
           ))}
         </ul>
         <button onClick={() => setPage(page + 1)}>Next Page</button>
@@ -66,6 +109,58 @@ function App() {
           onChange={(e) => setNewFirstName(e.target.value)}
         />
         <button onClick={updateFirstName}>Update First Name</button>
+      </div>
+
+      <div>
+        <h2>Add New Customer</h2>
+        <input
+          type="text"
+          placeholder="First Name"
+          value={addFirstName}
+          onChange={(e) => setAddFirstName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="First Last"
+          value={addLastName}
+          onChange={(e) => setAddLastName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Company Name"
+          value={addCompany}
+          onChange={(e) => setAddCompany(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Email Address"
+          value={addEmail}
+          onChange={(e) => setAddEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Phone Number"
+          value={addPhone}
+          onChange={(e) => setAddPhone(e.target.value)}
+        />
+        <button onClick={addNewCustomer}>Add New Customer</button>
+      </div>
+
+      <div>
+        <h2>Remove Customer</h2>
+        <input
+          type="text"
+          placeholder="Customer ID"
+          value={removeCustomerId}
+          onChange={(e) => setRemoveCustomerId(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="First Name"
+          value={removeFirstName}
+          onChange={(e) => setRemoveFirstName(e.target.value)}
+        />
+        <button onClick={removeExistingCustomer}>Remove Customer</button>
       </div>
     </>
   );
--- src/apiService.js
+++ src/apiService.js
@@ -6,13 +6,13 @@ export const fetchCustomers = async (sortBy, order, page, limit) => {
     limit: limit.toString(),
   }).toString();
 
-  const response = await fetch(
-    `https://my-first-node-web-app.azurewebsites.net/customers/?${queryParams}`
-  ).catch((error) => console.error("Error updating customer:", error));
+    const response = await fetch(
+      `https://my-first-node-web-app.azurewebsites.net/customers/?${queryParams}`
+    ).catch((error) => console.error("Error updating customer:", error));
 
-    // const response = await fetch(
-    //   `http://localhost:3005/customers?${queryParams}`
-    // ).catch((error) => console.error("Error updating customer:", error));
+//   const response = await fetch(
+//     `http://localhost:3005/customers?${queryParams}`
+//   ).catch((error) => console.error("Error updating customer:", error));
 
   const data = await response.json();
   console.log(data);
@@ -20,17 +20,17 @@ export const fetchCustomers = async (sortBy, order, page, limit) => {
 };
 
 export const updateCustomerFirstName = async (id, firstName) => {
-    const response = await fetch(
-      `https://my-first-node-web-app.azurewebsites.net/customers/${id}`,
-      {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          // "Authorization": "Bearer your_token" // Uncomment if authorization is needed
-        },
-        body: JSON.stringify( {firstName: firstName} )
-      }
-    ).catch((error) => console.error("Error updating customer:", error));
+  const response = await fetch(
+    `https://my-first-node-web-app.azurewebsites.net/customers/${id}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        // "Authorization": "Bearer your_token" // Uncomment if authorization is needed
+      },
+      body: JSON.stringify( {firstName: firstName} )
+    }
+  ).catch((error) => console.error("Error updating customer:", error));
 
 //   const response = await fetch(`http://localhost:3005/customers/${id}`, {
 //     method: "PUT",
@@ -39,6 +39,53 @@ export const updateCustomerFirstName = async (id, firstName) => {
 //       // "Authorization": "Bearer your_token" // Uncomment if authorization is needed
 //     },
 //     body: JSON.stringify({ firstName: firstName }),
+//   }).catch((error) => console.error("Error updating customer:", error));
+
+//   if (!response.ok) {
+//     throw new Error("Network response was not ok");
+//   }
+
+  const data = await response.json();
+  console.log(data);
+  alert(data.message);
+  window.location.reload();
+};
+
+export const addCustomer = async (
+  firstName,
+  lastName,
+  company = null,
+  email = null,
+  phone = null
+) => {
+  const response = await fetch(`https://my-first-node-web-app.azurewebsites.net/customers/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // "Authorization": "Bearer your_token" // Uncomment if authorization is needed
+      },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        company: company,
+        email: email,
+        phone: phone
+      }),
+    }).catch((error) => console.error("Error updating customer:", error));
+
+//   const response = await fetch(`http://localhost:3005/customers/`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       // "Authorization": "Bearer your_token" // Uncomment if authorization is needed
+//     },
+//     body: JSON.stringify({
+//       firstName: firstName,
+//       lastName: lastName,
+//       company: company,
+//       email: email,
+//       phone: phone,
+//     }),
 //   }).catch((error) => console.error("Error updating customer:", error));
 
   if (!response.ok) {
@@ -50,4 +97,35 @@ export const updateCustomerFirstName = async (id, firstName) => {
   window.location.reload();
 };
 
-export const addCustomer = async () => {};
+export const removeCustomer = async (id, firstName) => {
+
+  const response = await fetch(`https://my-first-node-web-app.azurewebsites.net/customers/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        // "Authorization": "Bearer your_token" // Uncomment if authorization is needed
+      },
+      body: JSON.stringify({
+        firstName: firstName,
+      }),
+    }).catch((error) => console.error("Error updating customer:", error));
+
+//   const response = await fetch(`http://localhost:3005/customers/${id}`, {
+//     method: "DELETE",
+//     headers: {
+//       "Content-Type": "application/json",
+//       // "Authorization": "Bearer your_token" // Uncomment if authorization is needed
+//     },
+//     body: JSON.stringify({
+//       firstName: firstName
+//     }),
+//   }).catch((error) => console.error("Error updating customer:", error));
+
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  const data = await response.json();
+  console.log(data);
+  alert(data.message);
+  window.location.reload();
+};
